11bd46e27f03b3d9de54c31bfc0762b681886af2
darole77 <142713605+darole77@users.noreply.github.com>
2023-08-30 13:36:57 +0200

    Update Grundlagen_der_Arithmetik.py

--- Grundlagen_der_Arithmetik.py
+++ Grundlagen_der_Arithmetik.py
@@ -3,27 +3,33 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 
+
 st.title("Grundlagen der Arithmetik")
 
 Kapitel=st.sidebar.radio("Wähle ein Kapitel", options=("Die natürlichen Zahlen", "Vom Zählen zum Rechnen", "Die vier Grundrechenarten", "Teilbarkeit", "Teilbarkeitsregeln", "Die ganzen Zahlen", "Rechenregeln für ganze Zahlen", "Die rationalen Zahlen", "Bruch- und Dezimalschreibweise", "Rechenregeln für rationale Zahlen", "Die reellen Zahlen", "Anordnung und Betrag" ))
 
+
+
 if Kapitel == "Die natürlichen Zahlen":
     st.header("Die natürlichen Zahlen $$\mathbb{N}$$")
     st.markdown("[Learningsnack](https://www.learningsnacks.de/share/249432/c7b260eb380b35c7fa7b6072f4e2767f1a45bab3)")
-    st.markdown("Das Zählen ist die grundlegendste mathematische Tätigkeit und stand vermutlich auch im historischen Kontext am Anfang der Mathematik. Ausgehend von der Einheit (die Zahl $$1$$) entstehen durch fortlaufendes Hinzufügen weiterer Einheiten die so genannten natürlichen Zahlen $$1, 2, 3, 4, 5, …$$")
+    st.markdown("Das Zählen ist die grundlegendste mathematische Tätigkeit und stand vermutlich auch im historischen Kontext am Anfang der Mathematik. Ausgehend von der Einheit (die Zahl $$\small{1}$$) entstehen durch fortlaufendes Hinzufügen weiterer Einheiten die so genannten natürlichen Zahlen $$\small{1, 2, 3, 4, 5, …}$$")
     
     image_url = "https://imageshack.com/i/pm5BDdqup"
     st.image(image_url)
        
     st.markdown("Die Menge der natürlichen Zahlen wird in der Mathematik mit dem Symbol N bezeichnet und man schreibt: ")
     st.latex(r"\mathbb{N} = \lbrace 1; 2; 3; 4; 5; ... \rbrace")
-    st.markdown("Dabei symbolisieren die drei Punkte $$„…“$$, dass es immer in dieser Form weitergeht, ohne jemals abzubrechen. Die natürlichen Zahlen lassen sich als Punkte am so genannten Zahlenstrahl veranschaulichen. Die Zahl 0 wird normalerweise nicht zu den natürlichen Zahlen gezählt. Möchte man die Zahl $$0$$ dabeihaben, so schreibt man in der Regel: ")
+    st.markdown("Dabei symbolisieren die drei Punkte $$„…“$$, dass es immer in dieser Form weitergeht, ohne jemals abzubrechen. Die natürlichen Zahlen lassen sich als Punkte am so genannten Zahlenstrahl veranschaulichen. Die Zahl 0 wird normalerweise nicht zu den natürlichen Zahlen gezählt. Möchte man die Zahl 0 dabeihaben, so schreibt man in der Regel: ")
     st.latex(r"\mathbb{N}_{0} = \lbrace 1; 2; 3; 4; 5; ... \rbrace")
     
     st.markdown("Die natürlichen Zahlen lassen sich am so genannten 'Zahlenstrahl' darstellen: ")
     image_url = "https://imageshack.com/i/pomGWAZ5p"
     st.image(image_url)
 
+    
+    
+    
 if Kapitel == "Vom Zählen zum Rechnen":
     st.header("Vom Zählen zum Rechnen")
     st.markdown("Über das **Zählen** gelangt man irgendwann zwangsläufig zum **Rechnen**.")
@@ -31,47 +37,95 @@ if Kapitel == "Vom Zählen zum Rechnen":
     st.markdown("Zähle zunächst die Dinge in jedem der vier untenstehenden Bilder. Überlege dir anschließend, wie du vorgegangen bist. ")
     image_url = "https://imageshack.com/i/poiWHaJRj"
     st.image(image_url)
-    st.markdown("Das **Bündeln in Gruppen** ist eine einfache und naheliegende **Zählstrategie**. Man braucht nicht mehr alle Objekte einzeln zu zählen, sondern fasst sie in Gruppen zusammen. Von hier ist es dann nur noch ein kleiner Schritt zur ersten Rechenoperation, der **Addition** (Plusrechnung). Wenn man beispielsweise die Anzahl der Dinge im zweiten Bild (rechts oben) durch $$3+3+5=11$$ ermittelt hat, dann hat man genau diese Zähl-Strategie angewendet und ist dadurch direkt vom Zählen zum Addieren übergegangen. Das Bündeln in Gruppen ist eine einfache und naheliegende Zählstrategie. Man braucht nicht mehr alle Objekte einzeln zu zählen, sondern fasst sie in Gruppen zusammen. Von hier ist es dann nur noch ein kleiner Schritt zur ersten Rechenoperation, der Addition (Plusrechnung). Wenn man beispielsweise die Anzahl der Dinge im zweiten Bild (rechts oben) durch $$3+3+5=11$$ ermittelt hat, dann hat man genau diese Zähl-Strategie angewendet und ist dadurch direkt vom **Zählen zum Addieren** übergegangen. ")
-    st.markdown("Wenn die Objekte in günstigen Mustern angeordnet sind (z.B. Reihen und Spalten), dann kann man ihre Anzahl auch durch **Multiplikation** (Malrechnung) ermitteln. Im dritten Bild (links unten) die Anzahl ermitteln, indem man $$2\cdot5=10$$ rechnet. ")
-    st.markdown("Wenn man im vierten Bild (rechts unten) die Anzahl beispielsweise über $$3\cdot5-1=14$$ ermittelt hat, so hat man bereits eine Kombination aus verschiedenen Rechenoperationen angewendet.")
+    st.markdown("Das **Bündeln in Gruppen** ist eine einfache und naheliegende **Zählstrategie**. Man braucht nicht mehr alle Objekte einzeln zu zählen, sondern fasst sie in Gruppen zusammen. Von hier ist es dann nur noch ein kleiner Schritt zur ersten Rechenoperation, der **Addition** (Plusrechnung). Wenn man beispielsweise die Anzahl der Dinge im zweiten Bild (rechts oben) durch $$\small{3+3+5=11}$$ ermittelt hat, dann hat man genau diese Zähl-Strategie angewendet und ist dadurch direkt vom Zählen zum Addieren übergegangen. Das Bündeln in Gruppen ist eine einfache und naheliegende Zählstrategie. Man braucht nicht mehr alle Objekte einzeln zu zählen, sondern fasst sie in Gruppen zusammen. Von hier ist es dann nur noch ein kleiner Schritt zur ersten Rechenoperation, der Addition (Plusrechnung). Wenn man beispielsweise die Anzahl der Dinge im zweiten Bild (rechts oben) durch $$\small{3+3+5=11}$$ ermittelt hat, dann hat man genau diese Zähl-Strategie angewendet und ist dadurch direkt vom **Zählen zum Addieren** übergegangen. ")
+    st.markdown("Wenn die Objekte in günstigen Mustern angeordnet sind (z.B. Reihen und Spalten), dann kann man ihre Anzahl auch durch **Multiplikation** (Malrechnung) ermitteln. Im dritten Bild (links unten) die Anzahl ermitteln, indem man $$\small{2\cdot5=10}$$ rechnet. ")
+    st.markdown("Wenn man im vierten Bild (rechts unten) die Anzahl beispielsweise über $$\small{3\cdot5-1=14}$$ ermittelt hat, so hat man bereits eine Kombination aus verschiedenen Rechenoperationen angewendet.")
     st.markdown("Die **Addition (+)** ist also eine naheliegende Weiterentwicklung des Zählens und die **Multiplikation (∙)** ist eine Weiterentwicklung der Addition. Gemeinsam mit den zugehörigen Umkehroperationen **Subtraktion (–)** und **Division (:)** ergeben sich die **vier Grundrechenarten** der Arithmetik.")
 
+    
+    
+    
+    
 if Kapitel == "Die vier Grundrechenarten":
     st.header("Die vier Grundrechenarten")
    
     st.subheader("Addition")
-    st.markdown("Beim **Addieren** werden Zahlen zusammengezählt. Das hierfür verwendete Symbol (Rechenzeichen) ist das Pluszeichen (+).")
-    st.latex(r"\begin{matrix}12&+&3&=&15\\ & & & & \\Summand&plus&Summand&gleich&Summe\end{matrix}")
+    st.markdown("Beim **$$\mathrm{\color{green}{Addieren}}$$** werden Zahlen zusammengezählt. Das hierfür verwendete Symbol (Rechenzeichen) ist das Pluszeichen (+).")
+    st.latex(r"\begin{matrix}12&+&3&=&15\\ & & & & \\ \mathrm{\color{blue}{Summand}}&\mathrm{plus}&\mathrm{\color{blue}{Summand}}&\mathrm{gleich}&\mathrm{\color{blue}{Summe}}\end{matrix}")
 
     st.subheader("Multiplikation")
-    st.markdown("Beim **Multiplizieren** werden Zahlen vervielfacht. Das hierfür verwendete Symbol (Rechenzeichen) ist das Malzeichen (∙).")
-    st.latex(r"\begin{matrix}12&\cdot&3&=&36\\ & & & & \\Faktor&mal&Faktor&=&Produkt\end{matrix}") 
+    st.markdown("Beim **$$\mathrm{\color{green}{Multiplizieren}}$$** werden Zahlen vervielfacht. Das hierfür verwendete Symbol (Rechenzeichen) ist das Malzeichen (∙).")
+    st.latex(r"\begin{matrix}12&\cdot&3&=&36\\ & & & & \\\mathrm{\color{blue}{Faktor}}&\mathrm{mal}&\mathrm{\color{blue}{Faktor}}&\mathrm{gleich}&\mathrm{\color{blue}{Produkt}}\end{matrix}") 
 
     st.subheader("Subtraktion")
-    st.markdown("Beim **Subtrahieren** wird eine Zahl von einer anderen abgezogen. Das hierfür verwendete Symbol (Rechenzeichen) ist das Minuszeichen (–).")
-    st.latex(r"\begin{matrix}12&-&3&=&9\\ & & & & \\Minuend&minus&Subtrahend&gleich&Differenz\end{matrix}")
+    st.markdown("Beim **$$\mathrm{\color{green}{Subtrahieren}}$$** wird eine Zahl von einer anderen abgezogen. Das hierfür verwendete Symbol (Rechenzeichen) ist das Minuszeichen (–).")
+    st.latex(r"\begin{matrix}12&-&3&=&9\\ & & & & \\\mathrm{\color{blue}{Minuend}}&\mathrm{minus}&\mathrm{\color{blue}{Subtrahend}}&\mathrm{gleich}&\mathrm{\color{blue}{Differenz}}\end{matrix}")
 
     st.subheader("Division")
-    st.markdown("Beim **Dividieren** werden Zahlen geteilt. Das hierfür verwendete Symbol (Rechenzeichen) ist das Geteilt-Zeichen (:).")
-    st.latex(r"\begin{matrix}12&:&3&=&4\\ & & & & \\ Dividend&durch&Divisor&=&Quotient\end{matrix}") 
-
+    st.markdown("Beim **$$\mathrm{\color{green}{Dividieren}}$$** werden Zahlen geteilt. Das hierfür verwendete Symbol (Rechenzeichen) ist das Geteilt-Zeichen (:).")
+    st.latex(r"\begin{matrix}12&:&3&=&4\\ & & & & \\ \mathrm{\color{blue}{Dividend}}&\mathrm{durch}&\mathrm{\color{blue}{Divisor}}&\mathrm{gleich}&\mathrm{\color{blue}{Quotient}}\end{matrix}")
+    
+    st.markdown("")
+    st.markdown("")
+    st.markdown("")
     st.subheader("Vorrangregeln")
-    st.markdown("Die Rechenoperationen **Addition** und **Subtraktion** bezeichnet man als **Strichrechenarten** (+ und –). Sie gelten als Rechenarten erster Stufe.")
-    st.markdown("Die Rechenoperationen **Multiplikation** und **Division** bezeichnet man als **Punktrechenarten** (∙ und :). Sie gelten als Rechenarten zweiter Stufe.")
+    st.markdown("Die Rechenoperationen **$$\mathrm{\color{green}{Addition}}$$** und **$$\mathrm{\color{green}{Subtraktion}}$$** bezeichnet man als **$$\mathrm{\color{green}{Strichrechenarten}}$$** (+ und –). Sie gelten als Rechenarten erster Stufe.")
+    st.markdown("Die Rechenoperationen **$$\mathrm{\color{blue}{Multiplikation}}$$** und **$$\mathrm{\color{blue}{Division}}$$** bezeichnet man als **$$\mathrm{\color{blue}{Punktrechenarten}}$$** (∙ und :). Sie gelten als Rechenarten zweiter Stufe.")
     st.markdown("Für die Reihenfolge der Rechenoperationen gilt:")
-    st.markdown("1. Kommen in einer Rechnung ausschließlich Rechenarten derselben Stufe vor, so wird **VON LINKS NACH RECHTS** gerechnet.")
-    st.markdown("2. Kommen in einer Rechnung Rechenarten unterschiedlicher Stufe und eventuell auch Klammern vor, so gilt: **KLAMMER VOR PUNKT VOR STRICH!**")
+    st.markdown("1. Kommen in einer Rechnung ausschließlich Rechenarten derselben Stufe vor, so wird **$$\mathrm{\color{red}{\: von \: links \: nach \: rechts \:}}$$** gerechnet.")
+    st.markdown("2. Kommen in einer Rechnung Rechenarten unterschiedlicher Stufe und eventuell auch Klammern vor, so gilt: **$$\mathrm{\color{red}{\: Klammer \: vor \: Punkt \: vor \: Strich! \:}}$$**")
 
+    
+    
+    
+    
+    
 if Kapitel == "Teilbarkeit":
     st.header("Teilbarkeit")
     st.markdown("Die Teilbarkeit ist eine wichtige Eigenschaft der natürlichen Zahlen. Sie wird in der Mathematik folgendermaßen definiert:")
     st.markdown("Man sagt „Die Zahl $$a$$ ist Teiler von $$b$$“ oder „$$b$$ ist durch $$a$$ teilbar“, wenn es zu den natürlichen Zahlen $$a$$ und $$b$$ eine natürliche Zahl $$c$$ gibt, sodass gilt:")
     st.latex(r"b=a⋅c")
     st.markdown("Wenn $$a$$ Teiler von $$b$$ ist, so schreibt man: $$a | b$$   andernfalls  $$a ∤ b$$")
-    image_url = "https://imageshack.com/i/poTtumIhj"
-    st.image(image_url)
-    image_url = "https://imageshack.com/i/pm4oQ4DCj"
-    st.image(image_url)
+    
+    st.markdown("Beispiele:")
+    data = {
+    "Aussage": ["$$\small\mathrm{3 \: ist \: \color{green}ein \: \color{black}Teiler \: von \: 12}$$", 
+                "$$\small\mathrm{3 \: ist \: \color{red}nicht \: \color{black}Teiler \: von \: 14}$$", 
+                "$$\small\mathrm{5 \: ist \: \color{green}ein \: \color{black}Teiler \: von \: 65}$$",
+                "$$\small\mathrm{6 \: ist \: \color{red}nicht \: \color{black}Teiler \: von \: 32}$$"],
+    "Begründung": ["$$\small\mathrm{... \: weil \: es \: \color{green}eine \: \color{black}natürliche \: Zahl \: gibt \: (4), \: sodass \: gilt: 12=3 \cdot 4}$$", 
+                   "$$\small\mathrm{... \: weil \: es \: \color{red}keine  \: \color{black}natürliche \: Zahl \: c \: gibt, \: sodass \: gilt: 14=3 \cdot c}$$",
+                   "$$\small\mathrm{... \: weil \: es \: \color{green}eine \: \color{black}natürliche \: Zahl \: gibt \: (13), \: sodass \: gilt: 65=5 \cdot 13}$$",
+                   "$$\small\mathrm{... \: weil \: es \: \color{red}keine \: \color{black}natürliche \: Zahl \: c \: gibt, \: sodass \: gilt: 32=6 \cdot c}$$"],
+    "Formale Schreibweise":["$$\small\mathrm{3 \mid 12}$$", 
+                            "$$\small\mathrm{3 ∤ 14}$$",
+                            "$$\small\mathrm{5 \mid 65}$$",
+                            "$\small\mathrm{6 ∤ 32}$"],}
+    df = pd.DataFrame(data)
+    md_df = df.to_markdown(index=False)
+    st.markdown(md_df, unsafe_allow_html=True)
+    
+    st.markdown("")
+    st.markdown("")
+    st.markdown("Teilbarkeit durch 0:")
+    data = {
+    "Aussage": ["$$\small\mathrm{0 \: ist \: kein \: Teiler \: von \: 1}$$", 
+                "$$\small\mathrm{0 \: ist \: kein \: Teiler \: von \: 2}$$", 
+                "$$\small\mathrm{0 \: ist \: kein \: Teiler \: von \: 3}$$",
+                "$$\small\mathrm{...}$$"],
+    "Begründung": ["$$\small\mathrm{... \: weil \: es \: keine \: natürliche \: Zahl \: c \: gibt, \: sodass \: gilt: 1=0 \cdot c}$$", 
+                   "$$\small\mathrm{... \: weil \: es \: keine \: natürliche \: Zahl \: c \: gibt, \: sodass \: gilt: 2=0 \cdot c}$$",
+                   "$$\small\mathrm{... \: weil \: es \: keine \: natürliche \: Zahl \: c \: gibt, \: sodass \: gilt: 3=0 \cdot c}$$",
+                   "$$\small\mathrm{...}$$"],
+    "Formale Schreibweise":["$$\small\mathrm{0 ∤ 1}$$", 
+                            "$$\small\mathrm{0 ∤ 2}$$",
+                            "$$\small\mathrm{0 ∤ 3}$$",
+                            "$\small\mathrm{...}$"],}
+    df = pd.DataFrame(data)
+    md_df = df.to_markdown(index=False)
+    st.markdown(md_df, unsafe_allow_html=True)
+    
+    
     
 if Kapitel == "Teilbarkeitsregeln":
     st.header("Teilbarkeitsregeln")
